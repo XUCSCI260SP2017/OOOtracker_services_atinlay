cabaa00a0552a8768baa8007434e0c6bd3b15d26
Luke McNamee <lucas.mcnamee@gmail.com>
2017-04-03 10:22:08 -0400

    Luke | 4/3/2017 | Andre is dumb

---
+++ build/resources/main/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(user_name, password, first_name, last_name) VALUES ('admin', 'password', 'Jesus', 'Christ');
+INSERT INTO users(user_name, password, first_name, last_name) VALUES ('dan', 'password', 'Dan', 'Digithingy');
+INSERT INTO users(user_name, password, first_name, last_name) VALUES ('test', 'test', 'Testy', 'McTestFace');
+
+
+INSERT INTO messages(sender_user_name, rec_user_name, msg_subject, msg_body) VALUES ('admin','test','testsubject','this is the body of the test message, i hope you got this');

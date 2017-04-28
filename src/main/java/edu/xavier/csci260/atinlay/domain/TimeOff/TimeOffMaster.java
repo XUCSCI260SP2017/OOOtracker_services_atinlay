0906c31872dc28f799047b37cb26051b31dcebd3
burdickd1 <burdickd@xavier.edu>
2017-04-28 12:03:20 -0400

    Master time off|Derek Burdick

--- src/main/java/edu/xavier/csci260/atinlay/domain/TimeOff/TimeOffMaster.java
+++ src/main/java/edu/xavier/csci260/atinlay/domain/TimeOff/TimeOffMaster.java
@@ -4,7 +4,7 @@ package edu.xavier.csci260.atinlay.domain.TimeOff;
 /**
  * Created by derekburdick on 4/24/17.
  */
-public class TimeOffMaster {
+public class TimeOffMaster extends TimeOff {
     private TimeOff timeOff;
     private TimeOffReq timeOffReq;
     private TimeOffResponse timeOffResponse;
@@ -16,10 +16,28 @@ public class TimeOffMaster {
         this.timeOffResponse = timeOffResponse;
     }
 
-    public void decide(){
-        //check which fields are null and determine the outcome of the response.
 
+    public String getStatus() {
+        return status;
     }
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
+    public TimeOffReq getTimeOffReq() {
+        return timeOffReq;
+    }
+
+    public void setTimeOffReq(TimeOffReq timeOffReq) {
+        this.timeOffReq = timeOffReq;
+    }
+
+    public TimeOffResponse getTimeOffResponse() {
+        return timeOffResponse;
+    }
+
+    public void setTimeOffResponse(TimeOffResponse timeOffResponse) {
+        this.timeOffResponse = timeOffResponse;
+    }
 }

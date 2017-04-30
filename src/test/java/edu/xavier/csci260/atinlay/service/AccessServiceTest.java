4d52fe074c1266dc41f85bb664327121cba61eca
burdickd1 <burdickd@xavier.edu>
2017-04-30 19:36:09 -0400

    service test|Derek&Cary

--- src/test/java/edu/xavier/csci260/atinlay/service/AccessServiceTest.java
+++ src/test/java/edu/xavier/csci260/atinlay/service/AccessServiceTest.java
@@ -1,15 +1,35 @@
 package edu.xavier.csci260.atinlay.service;
 
+import edu.xavier.csci260.atinlay.dal.EmployeeDAOImpl;
+import edu.xavier.csci260.atinlay.domain.Employee;
+import edu.xavier.csci260.atinlay.domain.RoleEnum;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static edu.xavier.csci260.atinlay.domain.RoleEnum.MANAGER;
 import static org.junit.Assert.*;
 
 /**
  * Created by derekburdick on 4/30/17.
  */
 public class AccessServiceTest {
+
+    @Autowired
+    private EmployeeDAOImpl employeeDAO;
+
+    @Autowired
+    private AccessService accessService;
+
+    private Employee employee = new Employee(
+            new Long (1234),
+            "test@usr.com",
+            "password",
+            "derek",
+            "burdick",
+            true,
+            MANAGER );
     @Before
     public void setUp() throws Exception {
 
@@ -21,8 +41,10 @@ public class AccessServiceTest {
     }
 
     @Test
-    public void addEmployee() {
-
+    public void addEmployee() throws Exception {
+        accessService.addEmployee(employee.getId(),employee.getUsername(),
+        employee.getPassword(), employee.getFirst_name(), employee.getLast_name(),employee.getEnabled(), employee.getRole());
+        assertSame(employeeDAO.getEmployeeByUsername(employee.getUsername()), employee);
     }
 
     @Test
